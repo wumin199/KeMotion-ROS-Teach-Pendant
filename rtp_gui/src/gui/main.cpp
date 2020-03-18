838b97b9a3e7df695e9f8b08798c079ce70c8859
wumin199 <wumin199@126.com>
2020-03-18 13:39:35 +0800

    modify demo.launch for ease of use

--- rtp_gui/src/gui/main.cpp
+++ rtp_gui/src/gui/main.cpp
@@ -7,9 +7,11 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "rtp_gui_node");
   ros::NodeHandle gui_nh;
   ros::CallbackQueue move_group_cb_queue;
-  std::string move_group_name = "arm_er7";
+  std::string move_group_name;
   std::string move_group_desc = "robot_description";
   gui_nh.setCallbackQueue(&move_group_cb_queue);
+
+  gui_nh.getParam("move_group_name", move_group_name);//eg."arm_er7" or "arm_er12"
 
   moveit::planning_interface::MoveGroupInterface::Options move_group_options(move_group_name, move_group_desc, gui_nh);
   moveit::planning_interface::MoveGroupInterface move_group(move_group_options);
--- rtp_gui/src/nodes/main.cpp
+++ rtp_gui/src/nodes/main.cpp
@@ -7,10 +7,12 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "rtp_node", ros::init_options::AnonymousName);
   ros::CallbackQueue move_group_cb_queue;
 
-  std::string move_group_name = "arm_er7";
+  std::string move_group_name;
   std::string move_group_desc = "robot_description";
   ros::NodeHandle move_group_nh;
   move_group_nh.setCallbackQueue(&move_group_cb_queue);
+
+  move_group_nh.getParam("move_group_name", move_group_name);//eg."arm_er7" or "arm_er12"
 
   moveit::planning_interface::MoveGroupInterface::Options move_group_options(move_group_name, move_group_desc, move_group_nh);
   moveit::planning_interface::MoveGroupInterface move_group(move_group_options);
